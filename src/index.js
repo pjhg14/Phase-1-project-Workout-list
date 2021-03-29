329ebe84bebcf023906e7cc38da952434388e562
pj <pjhg14@gmail.com>
2021-03-29 14:11:57 -0400

    Added project details, added personal branch

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// This will allow users to create a workout to-do list.
+
+// User Stories (remember about CRUD):
+
+// MVP:
+// User will be able to:
+// User will be able to add exercises to their blank list
+// Update the number reps and sets
+// Update their checklist with what they’ve done
+// Delete exercises
+
+// Stretch goals:
+// Login and create user profile
+// Add a timer function 
+// Link up to the outside API to seed our database
+
+// Timeline:
+
+// Monday: make exercise API, first fetch request, create a blank list
+
+
+// Tuesday: update and delete requests
+
+
+// Wednesday: work on stretch goals, try finding outside API
+
+// Thursday: finish goals, CSS
